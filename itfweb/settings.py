ce1724c36f69eb15b7181a1fa4716bb96092bbff
Gita Permatasari Sujatmiko <gitasujatmiko@gmail.com>
2020-02-09 21:44:44 +0700

    Tidur

--- itfweb/settings.py
+++ itfweb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'timeline-web.herokuapp.com']
 
 
 # Application definition
