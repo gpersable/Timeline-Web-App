af305845c4e728124fd65b86b0ccfbeccb7f21af
Gita Permatasari Sujatmiko <gitasujatmiko@gmail.com>
2020-02-24 05:49:14 +0700

    Update settings

--- itfweb/itfweb/settings.py
+++ itfweb/itfweb/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SCRET_KEY = os.environ.get('SECRET_KEY', True)
+SECRET_KEY = os.environ.get('SECRET_KEY', True)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
